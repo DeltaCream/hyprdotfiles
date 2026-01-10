36e2743a2967aa9cce58d69f7c5e25a3fc257dd9
DeltaCream <81692797+DeltaCream@users.noreply.github.com>
2026-01-10 14:41:02 +0800

    cargo install script, waypaper and zed
    - added typst-cli to cargo_install_script, marking it the first crate to be added in the install script
    - waypaper ini files changed:
    a. old config.ini file was renamed to example.ini, as it was an example configuration file sourced from waypaper's documentation
    b. the config that I had was then saved on the current config.ini
    - added zed settings on settings.json

--- scripts/cargo_install_script.sh
+++ scripts/cargo_install_script.sh
@@ -4,3 +4,6 @@ set -euo pipefail
 
 # This script is meant for Rust/cargo installations, both libraries and binaries.
 # As such, this will be separated between binaries and libraries.
+
+# typst
+cargo install --locked typst-cli
